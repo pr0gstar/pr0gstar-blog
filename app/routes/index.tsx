d3f605a7c594a9ce54ece151b35141506d82d69d
Christoph Planken <c.planken@absturzsicherung.de>
2022-04-19 12:43:44 +0200

    added markdown posts from Database

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,12 @@
+import { Link } from "@remix-run/react";
+
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, san-serif", lineHeight: "1.4" }}>
       <h1>Mein Blog</h1>
+      <Link to="/posts" className="text-xl1 text-blue-600 underline">
+        Blog Posts
+      </Link>
     </div>
   );
 }
